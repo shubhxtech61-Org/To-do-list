610e6f8b4f8e4a545fdd26b4a6417329d0eb142b
Shubham Tiwari <shubhxtech61@gmail.com>
2026-07-05 00:43:59 +0530

    Update settings.py

--- todo_main/settings.py
+++ todo_main/settings.py
@@ -115,5 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+import os
+
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
